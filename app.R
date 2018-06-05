c5694ec8ad4a79ec8fe8f925fc14b9ada4eaacf4
emmamclarnon <35924275+emmamclarnon@users.noreply.github.com>
2018-06-05 08:04:39 +0100

    D3 chart added - click bubbles
    Click bubbles to display a DT that is filtered according to the bubble clicked.

--- app.R
+++ app.R
@@ -17,7 +17,9 @@ library(lubridate)
 library(readr)
 library(dplyr)
 library(tidyverse)
-setwd("C:/Users/AT003502/Desktop/datasciacc/deskpro/")
+library(r2d3)
+library(shiny.router)
+#setwd("C:/Users/AT003502/Desktop/datasciacc/js")
 
 #function to import data and get rid of nas
 #table_import <- function (dataframe){
@@ -160,6 +162,11 @@ join = df_average %>%
   filter (!is.na(Service))%>%
   left_join (hourly_agg, by = "Service")
 
+
+
+
+# Creates router. We provide routing path and UI for this page.
+
 # join2 = join %>%
 #   mutate(test= med - hour_hits)
 # 
@@ -170,31 +177,35 @@ join = df_average %>%
 #     TRUE ~ "white"))
 
 #add the additional data to the table to include for next time
-df.append <- df %>%
-  rbind(time.data) %>%
-  filter (!is.na(date_hour))
+#df.append <- df %>%
+#  rbind(time.data) %>%
+ # filter (!is.na(date_hour))
 
-df.append <- df.append %>%
-  select(Timestamp, Service, Referrer.URL, Ticket, Count)
+#df.append <- df.append %>%
+#  select(Timestamp, Service, Referrer.URL, Ticket, Count)
 
 #write to the csv
-write.table(df.append, "test2.csv", sep = ",", col.names = F ,append = T)
+#write.table(df.append, "test2.csv", sep = ",", col.names = F ,append = T)
 
 # Define UI for application that draws a histogram
-ui <- dashboardPage(
+# Define UI for application that draws a histogram
+
+ui <- 
+dashboardPage(
   dashboardHeader(title="Deskpro Dashboard"),
-  dashboardSidebar(radioButtons("radio", label=("Choose average:"),
+  dashboardSidebar(radioButtons("radio", label=("Choose average type:"),
                                 choices=list("Mean"=1, "Median"=2, "Mode" =3), selected=2
   ),
   sidebarMenu(
     menuItem("Services", tabName = "services"),
-    menuItem("URL Table", tabName="urls")
+    menuItem("URL Table", tabName="urls"),
+    menuItem(bookmarkButton())
   )),
   dashboardBody(
     tabItems(
       tabItem("services",
               tags$style(HTML("
-
+                              
                               
                               .box.box-solid.box-primary>.box-header {
                               color:#ffffff;
@@ -210,32 +221,39 @@ ui <- dashboardPage(
                               ")),
               fluidRow(
                 theme = shinythemes::shinytheme("cerulean"),
-                box(status = "primary", solidHeader = TRUE,
-                    title = "Number of tickets raised per service",bubblesOutput("packagePlot", width = "100%", height = 600))
-                # ),
+                box(width=12,status = "primary", solidHeader = TRUE,
+                    title = "Number of tickets raised per service",d3Output("packagePlot", width = "100%", height = 700),click="plot_click"),
+                #  ),
+                fluidRow(
+                  DT::dataTableOutput("results")
+                )
+                #box(htmlOutput("x_value"),
+                #   verbatimTextOutput("selected_rows"))
               )
-    ),
-    tabItem("urls",
-            box(solidHeader=T,selectInput("Service","Service", 
-                                          c("All", unique(as.character(df_url$Service)))),
-                dateRangeInput('dateRange', label = h4('Date Range'), start = "2018-01-01", end = Sys.Date()), #Calendar for the user to input date
-                
-                DT::dataTableOutput("table"))
-            
-            )
-
-   )
-  )  
+              ),
+      tabItem("urls",
+              box(width =12, solidHeader=T,selectInput("Service","Service", 
+                                                       c("All", unique(as.character(df_url$Service)))),
+                  dateRangeInput('dateRange', label = h4('Date Range'), start = "2018-01-01", end = Sys.Date()), #Calendar for the user to input date
+                  
+                  DT::dataTableOutput("table"))
+              
+      )
+      
+      )
+    )  
 )
+
 #)
 #)
 #adds header and sidebar options - to add extra pages to the sidebar
+#adds header and sidebar options - to add extra pages to the sidebar
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output,session) {
 
   # Service <-(join2$Service)
-   output$packagePlot <- renderBubbles({
+   output$packagePlot <- renderD3({
      if (input$radio==1){
        join2= join %>%
          select(Service, date, mean, hour_hits)
@@ -247,6 +265,7 @@ server <- function(input, output) {
            test <0 ~ "red",
            TRUE ~ "lightgray"))
        join2
+       
      } 
      if (input$radio==2){
        join2= join %>%
@@ -273,6 +292,7 @@ server <- function(input, output) {
            TRUE ~ "lightgray"))
      
      join2
+    
    }
      join2
      order <- unique(join2$Service)
@@ -282,10 +302,20 @@ server <- function(input, output) {
        arrange(desc(n), tolower(Service)) %>%
   #     # Just show the top 60, otherwise it gets hard to see
        head(30)
-  #
-     bubbles(bub$n, bub$Service, key = bub$Service, color=bub$flags, tooltip =(bub$n) )
+     bub= bub %>%
+       select(Service,n) %>%
+       rename(id=Service,
+                 value=n)
+     bub=as.data.frame(bub)
+     write.csv(bub, "bub.csv", row.names=F)
+     r2d3(data = read.csv("bub.csv"), d3_version = 4, script = "bubbles.js")
+     
+     #bubbles=bubbles(bub$n, bub$Service, key = bub$Service, color=bub$flags, tooltip =(bub$n) )
+
+
   #   #bubbles(hourly_url_agg$Service, hourly_url_agg$Service, key = hourly_url_agg$Service)
    })
+   
 
   #uses historical data
   df_url
@@ -324,7 +354,7 @@ server <- function(input, output) {
     df_url
     if (input$radio==3){
          df_url= df_url %>%
-             select(Service, Date, Mean, Total)
+             select(Service, Date, Mode, Total)
     }
     
     df_url
@@ -335,26 +365,32 @@ server <- function(input, output) {
     df_url
   
     df_url=df_url[df_url$Date>=(input$dateRange[1]) & df_url$Date<=(input$dateRange[2]),]
-    
+    df_url
    
   })
+
   )
+  
+    wc_click <- reactive({
+    a <- input$clickedValue
+    print(a)
+  })
+  data <- reactive({ 
+    val <- wc_click()
+    if(!is.null(val))
+    {
+      data <-filter(df_url,Service==val)
+    }
+  })
+  output$results <-  DT::renderDataTable({
+    data()
+  
+  })
+  
+    
+  
 
-    # 
-    # serv
-    # if (input$radio==2){
-    #   serv= df_url %>%
-    #     select(Service, Date, Median, Total)
-    # }
-    # serv
-    # if (input$radio==3){
-    #   serv= df_url %>%
-    #     select(Service, Date, Mode, Total)
-    # }
-    # serv
-
-
-  #format the table using reported issues column, any value above the mean
+  #format the table using repor ted issues column, any value above the mean
   #colour it in red
 #  %>%formatStyle(
   # 'col',
@@ -362,30 +398,7 @@ server <- function(input, output) {
  # )
   #)
 #)
-
-  
-# output$ticketChart<-renderPlotly({
- #   plot_ly(df,
- #           x=df$date,
-  #          y=df$Count,
-  #          type="scatter",
-  #          mode="lines",
-           # linetype= df$Service,
-  #          showlegend = FALSE)%>%
-      #hoverinfo="text",
-      #text=paste(repo,"<br>",created_at,"<br> ",title),
-      #marker=list(color=colStatus)) %>%
-      
-  #    layout(hovermode = "closest",
-   #          xaxis=list(title="",tickangle=55, ticklen=3, dtick=15, ticks="outside"), font=list(family="arial", size=10),
-    #         yaxis=list(title="Volume of tickets raised per hour"), font=list(family="arial"),
-   #          title="Deskpro Tickets",
-   #          titlefont=list(size=16, family="arial"),
-    #         margin=list(b=100,r=50)
-    #  )
-    
-  #}) 
 }
 # Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server, enableBookmarking="url")
 
